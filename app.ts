2a22faa8258775b98c65cfdac2a287561eee41a1
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-01 21:30:06 -0600

    typeof

--- app.ts
+++ app.ts
@@ -4,10 +4,14 @@ console.log('hellow world 21');
 // boolean
 
 function add(n1:number,n2:number):number{
+    console.log(typeof n1);
+    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error('incorrect input');
+    }
     return n1+n2;
 }
 
-const number1 = 5;
+const number1 = "5";
 const number2 = 2.89;
 
 const result = add(number1,number2);
