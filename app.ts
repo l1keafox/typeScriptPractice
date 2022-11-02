a73ba06433522bd4cbb2290932c23c96d0e31e68
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-01 21:26:05 -0600

    core

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('hellow world 2');
+console.log('hellow world 21');
+// number
+// string - 
+// boolean
+
+function add(n1:number,n2:number):number{
+    return n1+n2;
+}
+
+const number1 = 5;
+const number2 = 2.89;
+
+const result = add(number1,number2);
+console.log(result);
