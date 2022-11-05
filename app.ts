f2f18b2dec015270bc56e921e4134d33cab5fc46
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-04 20:43:40 -0600

    Types

--- app.ts
+++ app.ts
@@ -1,6 +1,15 @@
-const person:object = {
-    name: "ray",
-    age:30
-};
+function combine(input1:number | string , input2:number | string, typeOfResult:string){
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
 
-console.log(person);
+    return result;
+}
+
+
+const cominedAges = combine(30,20, 'as-number');
+console.log(cominedAges);
+const cominedAges2 = combine("Max","Anna", 'as-text');
